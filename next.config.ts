a5ba35528076fc59b6b4df3eec657f29bdcaee40
zerosatin121 <mathu.muchoki@gmail.com>
2025-12-27 13:45:27 +0300

    Initialize project configuration and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'books.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'books.google.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
